ff48cde4e75a7856e88471f4ccb12d7c7bf5fe8a
bayirta <100474513+bayirta@users.noreply.github.com>
2022-03-10 00:08:53 +0300

    Update 03_hw.py

--- Module3/home_work/03_hw.py
+++ Module3/home_work/03_hw.py
@@ -2,3 +2,9 @@
 # Вывести на экран сумму всех положительных элементов кратных двум.
 
 # TODO: your code here
+numbers_list = [10, -7, 95, 62, -38]
+even_positive_sum = 0
+for number in numbers_list:
+    if number > 0 and number % 2 == 0:
+        even_positive_sum += number
+print(even_positive_sum)
