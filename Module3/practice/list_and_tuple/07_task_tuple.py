e894e74694120872c5c45a620a880016ce489fd7
bayirta <100474513+bayirta@users.noreply.github.com>
2022-03-12 12:10:22 +0300

    Update 07_task_tuple.py

--- Module3/practice/list_and_tuple/07_task_tuple.py
+++ Module3/practice/list_and_tuple/07_task_tuple.py
@@ -3,3 +3,9 @@
 tup = (2, 4, 6, -4, 12, 0, 5)
 
 # TODO: your code here
+new_list = list(tup)
+max_value = int()
+for el in new_list:
+    if el > max_value:
+        max_value = el
+print(max_value)
