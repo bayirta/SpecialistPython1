4c6c5812a5fe38366bb6ef5ea625af2707755271
bayirta <100474513+bayirta@users.noreply.github.com>
2022-03-18 00:03:19 +0300

    Update 02_hw_func.py

--- Module4/home_work/02_hw_func.py
+++ Module4/home_work/02_hw_func.py
@@ -3,11 +3,31 @@
 # Если отрезков с минимальной длиной несколько - вывести любой
 
 # При решении задачи необходимо использовать функцию расстояния между двумя точками.
-
 def distance(x1, y1, x2, y2):
-    # TODO: тело, которое вы реализовали на практической работе
-    pass
+       return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
+A = (-3, -1)
+B = (2, 4)
+C = (1, -3)
 
-# TODO: your code here
-print("Самый короткий отрезок:", ...)  # Выводим название отрезка, например “АС”.
+list_of_dist = [
+    {
+        'name': 'AB',
+        'distance': distance(*A, *B)
+    },
+    {
+        'name': 'BC',
+        'distance': distance(*B, *C)
+    },
+    {
+        'name': 'AC',
+        'distance': distance(*A, *C)
+    }
+]
+
+min_segm = list_of_dist[0]
+for segment in list_of_dist:
+    if segment['distance'] < min_segm['distance']:
+        min_segm = segment
+
+print("Самый короткий отрезок:", min_segm['name'])  # Выводим название отрезка, например “АС”.
