92597b0cddbecc6b5d3ba0b781c658983d9ca350
bayirta <100474513+bayirta@users.noreply.github.com>
2022-03-18 01:00:09 +0300

    Update 03_hw_func.py

--- Module4/home_work/03_hw_func.py
+++ Module4/home_work/03_hw_func.py
@@ -2,3 +2,13 @@
 # Находится ли одна окружность целиком внутри другой
 
 # TODO: your code here
+def distance(x1, y1, x2, y2):
+    return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+
+def circle_in_circle(center1, center2, R1, R2):
+   return distance(*center1, *center2) < abs(R2 - R1)
+
+print(circle_in_circle((-2, 1), (-1.75, 1), 2, 1.75))
+print(circle_in_circle((-2, 1), (-1.5, 1), 2, 1))
+print(circle_in_circle((-2, 1), (2, 1), 2, 2))
+print(circle_in_circle((-2, 1), (1, 2), 2, 2))
