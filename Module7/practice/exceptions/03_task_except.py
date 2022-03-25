6715d72b51ced37d228b3176284aad4d443459a8
bayirta <100474513+bayirta@users.noreply.github.com>
2022-03-26 00:59:07 +0300

    Update 03_task_except.py

--- Module7/practice/exceptions/03_task_except.py
+++ Module7/practice/exceptions/03_task_except.py
@@ -5,3 +5,30 @@
 
 # При решении задачи требуется учесть формат входных данных.
 # Если входные данные некорректные, сообщить об этом.
+while True:
+    raw_data = input("Введите 5 целых чисел через пробел ")
+    try:
+        list_of_el = raw_data.split()
+        list_of_int = []
+        for el in list_of_el:
+            if len(list_of_el) == 5:
+                list_of_int.append(int(el))
+            else:
+                raise ValueError
+        break
+    except ValueError:
+        print("Некорректные данные")
+list_of_pos_int = []
+for int_el in list_of_int:
+    if int_el > 0:
+        list_of_pos_int.append(int_el)
+# print(list_of_pos_int)
+try:
+    list_of_pos_int != []
+    min_int = list_of_pos_int[0]
+    for int_pos_el in list_of_pos_int:
+        if int_pos_el < min_int:
+            min_int = int_pos_el
+    print(min_int)
+except:
+    print("Положительных чисел нет")
